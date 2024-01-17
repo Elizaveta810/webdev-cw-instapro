e82161bddea7f2d83c679963f45ea97173949bf7
Elizaveta810 <elizaveta.k810@mail.ru>
2024-01-17 11:52:19 +0300

    сформировала шаблон постов

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,16 +1,30 @@
+import { renderUploadImageComponent } from "./upload-image-component.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
+  let imageUrl = "";
   const render = () => {
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
       Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
+      <div class="upload-image-container"></div>
+       <button class="button" id="add-button">Добавить</button>
     </div>
   `;
 
     appEl.innerHTML = appHtml;
+    const uploadImageContainer = appEl.querySelector(".upload-image-container");
 
+    if (uploadImageContainer) {
+      renderUploadImageComponent({
+        element: appEl.querySelector(".upload-image-container"),
+        onImageUrlChange(newImageUrl) {
+          imageUrl = newImageUrl;
+        },
+      });
+    }
+//повесить Клик на кнопку добавить и добавить поле ввода комментария!!!!!!!!!!
     document.getElementById("add-button").addEventListener("click", () => {
       onAddPostClick({
         description: "Описание картинки",
